4d57bbe4cb99fa5cba640963fc5c4643cdad4221
xiaoji121 <dong.fly.ming@gmail.com>
2013-04-09 22:01:43 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+require.config({
+  paths: {
+    'jquery': 'lib/jquery'
+  },
+  shim: {
+    'lib/underscore': {
+      exports: '_'
+    },
+    'lib/backbone': {
+      deps: ["lib/underscore", "jquery"],
+      exports: 'Backbone'
+    }
+  }
+});
+// items 是一个全局的变量，在整个APP中都能调得到
+var items = [
+  { title: "Macbook Air", price: 799 },
+  { title: "Macbook Pro", price: 999 },
+  { title: "The new iPad", price: 399 },
+  { title: "Magic Mouse", price: 50 },
+  { title: "Cinema Display", price: 799 }
+];
+require(
+  ["jquery",
+    "lib/underscore",
+    "lib/backbone",
+    "views/cartcollectionview"
+  ],
+  function($, _, B, CartCollectionView) {
+    $(function() {
+      new CartCollectionView(items);
+    });
+  }
+);
+
